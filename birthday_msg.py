b8c96c7b4aa26ab67b4636c18bbfd35939e798ad
freebird <nathanfreeman@outlook.com>
2026-02-11 15:24:25 -0500

    new python file

---
+++ birthday_msg.py
@@ -0,0 +1,5 @@
+name = "Georgie"
+print(f"Happy Birthday to {name}")
+
+
+# printf("Happy Birthday to %s", name)
---
+++ variables.py
@@ -0,0 +1,5 @@
+#this is a comment
+#this is also a comment
+#this is my last comment
+
+print("hello Sam \#")
