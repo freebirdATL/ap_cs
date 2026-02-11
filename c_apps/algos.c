cc50f4f7f76414138b786e98405754e7997cefc2
freebird <nathanfreeman@outlook.com>
2026-02-11 14:54:43 -0500

    new python file

--- c_apps/algos.c
+++ c_apps/algos.c
@@ -1,24 +1,15 @@
 #include <stdio.h>
 
-int main() {
-    int a = 97;
-    int b = 'b';
-    //int c = "c"; //incompatible pointer to integer conversion
-    char d = a;
-    char e = (char)a;
-    printf(
-        "a: %d\n"
-        "b: %d\n"
-        "b: %c\n"sadfsdfls
-        // "c: %d\n"
-        // "c: %c\n"
-        // "c: %s\n"
-        "d: %d\n"
-        "d: %c\n"
-        // "d: %s\n"
-        "e: %d\n"
-        "e: %c\n"
-        //"e: %s\n"
-        ,a,b,b,d,d,e,e
-    );
+
+int main()
+{
+    int x = 100;
+    int y = 50;
+
+    if (x > y) {
+        printf("Alpha");
+    } else {
+        printf("Beta");
+    }
+
 }
