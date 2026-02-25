dca51f7502a017f60c542c771a64caca8d297308
freebird <nathanfreeman@outlook.com>
2026-02-25 14:24:44 -0500

    update functions

---
+++ functions.py
@@ -0,0 +1,5 @@
+
+def my_func(param1, param2):
+    output = param1 + param1
+    print(output)
+
--- variables.py
+++
@@ -1,5 +0,0 @@
-#this is a comment
-#this is also a comment
-#this is my last comment
-
-print("hello Sam \#")
